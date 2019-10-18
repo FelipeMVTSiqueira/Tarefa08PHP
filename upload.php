c4e6038ca040fd6fa67a86f7c6dfd038ec053add
Felipe Siqueira <felipesiqueira1212@gmail.com>
2019-10-17 23:50:27 -0300

    Início do Ex 1)

---
+++ upload.php
@@ -0,0 +1,29 @@
+
+<?php
+if($_POST){
+        $nome = $_FILES["enviar"]["name"];
+        $arquivoTemp = $_FILES["enviar"]["tmp_name"];
+        $caminho = "uploads/".$nome;
+        $movendo = move_uploaded_file($arquivoTemp, $caminho);
+        if($_FILES["enviar"]["error"] == UPLOAD_ERR_OK){
+            echo "Download feito com sucesso!";
+        }else {
+            echo "Um arquivo com esse nome já existe =/ Por favor tente um outro.";
+        }
+    }
+?>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Document</title>
+        </head>
+        <body>
+        <form action="" method="post" enctype="multipart/form-data">
+            Selecione arquivo para upload: <br><br><input type="file" name="enviar">
+            <button type="submit">Enviar</button>
+        </form>
+        </body>
+        </html>
